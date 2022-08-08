d2ea956e9f5d631d530806ba76e391a8c49653f8
AshenGod <jacobrawefi122@outlook.com>
2022-08-08 12:33:06 +0100

    first commit

---
+++ challengecomp/src/App.js
@@ -0,0 +1,20 @@
+import NavBar from './Comp/NavBar';
+import AboutMe from './Comp/AboutMe';
+import Projects from './Comp/Projects';
+import Contact from './Comp/Contact';
+import Footer from './Comp/Footer';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <NavBar/>
+   <AboutMe/>
+   <Projects/>
+   <Contact/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
